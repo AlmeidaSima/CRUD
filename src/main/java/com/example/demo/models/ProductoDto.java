b18cf762c30c41f3ae8fd89aba3e25b6930c4c95
AlmeidaSima <almeida.sima2000@gmail.com>
2025-11-03 15:05:57 -0500

    nuevo boton confirmacion
    Modal para boton de confirmacion

--- src/main/java/com/example/demo/models/ProductoDto.java
+++ src/main/java/com/example/demo/models/ProductoDto.java
@@ -20,7 +20,7 @@ public class ProductoDto {
 	@Min(0)
 	private double price;
 	
-	@Size(min = 10, message = "The description should be at least 10 characteres no te excedas mijo")
+	@Size(min = 10, message = "The description should be at least 10 characteres no se permiten mas")
 	private String description;
 	
 	private MultipartFile imageFile;
